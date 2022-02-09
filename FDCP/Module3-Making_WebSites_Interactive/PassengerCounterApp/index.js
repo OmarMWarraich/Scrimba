8c2c83da53248539221eeb82a59e2896aab617af
OmarMWarraich <earthling986@gmail.com>
2022-02-09 23:56:51 +0500

    Set the count to zero

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -1,21 +1,19 @@
-// 1. Grab the save-el paragrah and store it in a variable called saveEl
+
 let saveEl = document.getElementById("save-el")
 
 let countEl = document.getElementById("count-el")
 let count = 0
 
-console.log(saveEl)
+
 
 function increment() {
     count += 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
 function save() {
     let countStr = count + " - "
-    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
-    // 3. Render the variable in the saveEl using innerText
     saveEl.textContent += countStr
-    // NB: Make sure to not delete the existing content of the paragraph
-    console.log(count)
+    countEl.textContent = 0
+    count = 0
 }
