cd10d8a28463500067a951ad06abc05ff2f483c0
OmarMWarraich <earthling986@gmail.com>
2022-02-09 18:09:58 +0500

    passenger counter app

---
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -0,0 +1,64 @@
+// document.getElementById('count-el').innerText = 10
+
+
+
+// var myAge = 39
+
+// console.log(myAge)
+
+// Cannot access variable before initialization
+
+// let firstBatch = 5
+// let secondBatch = 7
+
+// let count = firstBatch + secondBatch
+
+
+// console.log(count)
+
+
+// let myAge = 39
+// let humanDogRatio = 7
+
+// let myDogAge = myAge * humanDogRatio
+
+// console.log(myDogAge)
+
+// /**
+//  * Assigining and Incrementing
+//  */
+
+// let count = 5
+
+// count = count + 3
+
+// count = count - 2
+
+// console.log(count)
+
+// /**
+//  * Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+//  *  Decrease it down to 25, and then finally increase it to 70
+//  */
+
+// var bonusPoints = 50
+// console.log(bonusPoints)
+// bonusPoints += 50
+// console.log(bonusPoints)
+// bonusPoints -= 75
+// console.log(bonusPoints)
+// bonusPoints += 45
+// console.log(bonusPoints)
+
+/**
+ * intialize the count as 0
+ * listen for clicks on the increment button
+ * increment the count variable when the button is clicked
+ * change the count - el in the HTML to reflect the new count
+ */
+
+
+function increment() {
+    console.log('The button was clicked')
+}
+let count = 0
