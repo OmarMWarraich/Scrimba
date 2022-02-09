0ecfb2a62cb8af7401b73b3cc5c711d030b683e1
OmarMWarraich <earthling986@gmail.com>
2022-02-09 19:32:48 +0500

    Strings vs number

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -4,7 +4,17 @@ let greeting = "Hi, my name is "
 
 let myGreeting = greeting + name + "!"
 
-console.log(myGreeting)
+// console.log(myGreeting)
 
 
+let point = 4
 
+let bonusPoints = "10"
+
+let totalPoints = point + bonusPoints
+
+console.log(totalPoints)
+console.log(4 + 5)
+console.log("2" + "4")
+console.log("5" + 1)
+console.log(100+100)
