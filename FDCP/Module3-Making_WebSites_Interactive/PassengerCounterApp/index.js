6c9f3c3336e5b515210e5666b2a0470ce43984ff
OmarMWarraich <earthling986@gmail.com>
2022-02-10 05:27:40 +0500

    Unit Project Completed
    This is a very basic project covering only html and js

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -1,35 +1,19 @@
-// let inputNum = document.getElementById('unitNum')
 
-// console.log(inputNum)
-
-// let val = document.getElementById('unitNum')
-// function inputNum() {
-//     let num = val.oninput
-//     console.log(num)
-// }
-
-// document.addEventListener('keydown', inputNum)
-
-
-
-// function submit() {
-//     btn.addEventListener('submit', (e) => {
-//         e.preventDefault();
-//         let val = num.value;
-//         console.log(val);
-//     })
-// }
-// submit();
 
 function main() {
-    var input = document.getElementsByClassName('insert');
+    let input = document.getElementsByClassName('insert');
 
     document.addEventListener('keydown', function (e) {
         if (window.event == 13 || e.which == 13) {
-            var num = input[0].value;
-            console.log(num);        
+            let num = input[0].value;
+            console.log(num);
+            document.getElementById("feet").textContent = (num + " meters = " + (num * 3.280).toFixed(2) + "feet" + " |" + num + " feet" + " =" + (num / 3.280).toFixed(2) + "meters");
+            document.getElementById("volume").textContent = (num + " litres = " + (num * 0.264).toFixed(2) + "gallons" + " |" + num + " gallons" + " =" + (num / 0.264).toFixed(2) + "litres");
+            document.getElementById("mass").textContent = (num + " kilos = " + (num*2.204).toFixed(2) + "pounds" +" |"+ num + " pounds" + " =" + (num/2.204).toFixed(2) + "kilos" );
+            
         }
     })
     
 }
-window.onload = main;
+
+main();
