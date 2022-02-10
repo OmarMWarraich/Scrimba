086463213b98e1dd1ac560e37433a327a5616d25
OmarMWarraich <earthling986@gmail.com>
2022-02-11 00:59:43 +0500

    Sorting fruits using for loop and conditional statements

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -1,10 +1,19 @@
-let hands = ["rock", "paper", "scissor"]
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
+let appleShelf = document.getElementById("apple-shelf")
+let orangeShelf = document.getElementById("orange-shelf")
 
-// Create a function that returns a random item from the array
+// Create a function that puts the apples onto the appleShelf
+// and the oranges onto the orangeShelf. Use a for loop,
+// a conditional statement, and the textContent property.
 
-function getHand() {
-    let randomIndex = Math.floor( Math.random() * 3)
-    return hands[randomIndex]
+function sortFruit() {
+    for (let i = 0; i < fruit.length; i++) {
+        if (fruit[i] === "🍎") {
+            appleShelf.textContent += "🍎"
+        } else if (fruit[i] === "🍊") {
+            orangeShelf.textContent += "🍊"
+        }
+    }
 }
 
-console.log( getHand() )
+sortFruit()
