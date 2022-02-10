e3e1cdbc0c39bf0d4b3e4837b77c82de1a69852d
OmarMWarraich <earthling986@gmail.com>
2022-02-11 00:51:21 +0500

    Rock, paper, scissor

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -1,66 +1,11 @@
-let cards = []
-let sum = 0
-let hasBlackJack = false
-let isAlive = false
-let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("card-el")
-
-let player = {
-    name : "Omar",
-    chips : 145
+let hands = ["rock", "paper", "scissor"]
+// Create a function that returns a random item from the array
+function getRandomItem(arr) {
+    // get random index value
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    // get random item
+    const item = arr[randomIndex];
+    return item;
 }
-
-let playerEl = document.getElementById("player-el")
-playerEl.textContent = player.name + ":  $" + player.chips
-
-function getRandomCard() {
-    let randomNumber = Math.floor(Math.random() * 13) + 1
-    if (randomNumber > 10) {
-        return 10
-    } else if (randomNumber === 1) {
-        return 11
-    }
-    else {
-        return randomNumber
-       }    
-}
-console.log(cards)
-
-function startGame() {
-    isAlive = true
-    let firstCard = getRandomCard()
-    let secondCard= getRandomCard()
-    cards = [firstCard, secondCard]
-    sum = firstCard + secondCard
-    renderGame()
-}
-
-function renderGame() {
-
-    cardsEl.textContent = "Cards: "
-    for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " "
-    }
-    sumEl.textContent = "Sum: " + sum
-    if (sum <= 20) {
-        message = "Do you want to draw a new card?"
-    } else if (sum === 21) {
-        message = "You've got Blackjack!"
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game!"
-        isAlive = false
-    }
-    messageEl.textContent = message
-}
-
-function newCard() {
-    if(isAlive === true && hasBlackJack === false){
-    let card = getRandomCard()
-    sum += card
-    cards.push(card)
-        renderGame()
-    }
-}
+const result = getRandomItem(hands);
+console.log(result);
