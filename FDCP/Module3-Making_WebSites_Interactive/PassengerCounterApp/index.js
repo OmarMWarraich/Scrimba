c285a61b3dd822f7f02bd39fbf934739857d1eab
OmarMWarraich <earthling986@gmail.com>
2022-02-11 00:52:57 +0500

    Get random item array

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -1,11 +1,10 @@
 let hands = ["rock", "paper", "scissor"]
+
 // Create a function that returns a random item from the array
-function getRandomItem(arr) {
-    // get random index value
-    const randomIndex = Math.floor(Math.random() * arr.length);
-    // get random item
-    const item = arr[randomIndex];
-    return item;
+
+function getHand() {
+    let randomIndex = Math.floor( Math.random() * 3)
+    return hands[randomIndex]
 }
-const result = getRandomItem(hands);
-console.log(result);
+
+console.log( getHand() )
