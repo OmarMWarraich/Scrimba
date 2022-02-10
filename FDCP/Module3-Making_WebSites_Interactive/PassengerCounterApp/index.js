11750f905faa3f540605f2a7a4a37e38e62614c6
OmarMWarraich <earthling986@gmail.com>
2022-02-10 16:31:16 +0500

    Initial commit: blackjack

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -1,33 +1,18 @@
-let num1 = 8
-let num2 = 2
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
+let firstCard = 10
+let secondCard = 7
+let sum = firstCard + secondCard 
+let hasBlackJack = false
+let isALive = true
 
-// Create four functions: add(), subtract(), divide(), multiply()
-// Call the correct function when the user clicks on one of the buttons
-// Perform the given calculation using num1 and num2
-// Render the result of the calculation in the paragraph with id="sum-el"
-
-// E.g. if the user clicks on the "Plus" button, you should render
-// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
-let sumEl = document.getElementById("sum-el")
-
-function add() {
-    let result = num1 + num2
-    sumEl.textContent = "Sum: " + result
+let message = ''
+if (sum < 21) {
+    message = "Do you want to draw a new card? 🙂"
+} else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳"
+    hasBlackJack = true
+} else {
+    message = "You're out of the game! 😭"
+    isALive = false
 }
 
-function subtract() {
-    let result = num1 - num2
-    sumEl.textContent = "Subtract: " + result
-}
-
-function divide() {
-    let result = num1 / num2
-    sumEl.textContent = "Divide: " + result
-}
-
-function multiply() {
-    let result = num1 * num2
-    sumEl.textContent = "Multiply: " + result
-}
+console.log(message)
