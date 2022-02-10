1b47ddbede2b4a7408eb059da765c465eb2c2000
OmarMWarraich <earthling986@gmail.com>
2022-02-10 16:47:43 +0500

    Blackjack: make the start button wotk

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -1,18 +1,22 @@
 let firstCard = 10
-let secondCard = 7
-let sum = firstCard + secondCard 
+let secondCard = 4
+let sum = firstCard + secondCard
 let hasBlackJack = false
-let isALive = true
+let isAlive = true
+let message = ""
 
-let message = ''
-if (sum < 21) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭"
-    isALive = false
+// 1. Store the message-el paragraph in a variable called messageEl
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+    // 2. Display the message in the messageEl using messageEl.textContent
+    console.log(message)    
 }
-
-console.log(message)
