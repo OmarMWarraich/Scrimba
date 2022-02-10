ae9607922ff33ad55b1cc7911632e92c59828a9d
OmarMWarraich <earthling986@gmail.com>
2022-02-10 17:40:14 +0500

    Blackjack: Display the cards
    There might have been some errors in the previous one or two commits

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -5,11 +5,13 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
-// 2. Store the sum paragraph in a variable called sumEl
 let sumEl = document.getElementById("sum-el")
+// 2. Store the cards paragraph in a variable called cardsEl
+let cardsEl = document.getElementById("card-el")
 
 function startGame() {
-    // 3. Render the sum on the page using this format -> "Sum: 14"
+    // 3. Render the sum on the page using this format -> "Cards: 10 4"
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard  
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
