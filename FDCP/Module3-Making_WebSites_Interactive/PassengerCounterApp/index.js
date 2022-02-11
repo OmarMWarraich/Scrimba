a685e17d21504e0d059efc74935ac43136aafd94
OmarMWarraich <earthling986@gmail.com>
2022-02-11 21:02:13 +0500

    Used creaeElement() and append() instead of innerHTML
    created element, set text content, appended to ul

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -10,6 +10,7 @@ inputBtn.addEventListener("click", function () {
 })
 
 for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-    
+    const li = document.createElement("li")
+    li.textContent = myLeads[i]
+    ulEl.append(li)
 }
