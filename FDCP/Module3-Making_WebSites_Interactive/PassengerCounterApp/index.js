919331507d0e01c24a19f5879427af3d1bbdd982
OmarMWarraich <earthling986@gmail.com>
2022-02-11 21:14:05 +0500

    Improved the performance of app
    Created a variable, listItems to hold all the HTML for list items, added the item to the listItems variable instead of the ulEl, rendered the listItems inside the unl using ulEl.innerHTML

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -9,8 +9,8 @@ inputBtn.addEventListener("click", function () {
     console.log(myLeads)
 })
 
+let listItems = ""
 for (let i = 0; i < myLeads.length; i++) {
-    const li = document.createElement("li")
-    li.textContent = myLeads[i]
-    ulEl.append(li)
+    listItems += "<li>" + myLeads[i] + "</li>"
 }
+ulEl.innerHTML = listItems
