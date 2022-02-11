d5372fbb34f0e8c29e983ec405595c286f8d3964
OmarMWarraich <earthling986@gmail.com>
2022-02-11 17:04:25 +0500

    Push the value from the input field

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -4,5 +4,6 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function () {
-    console.log("Button clicked from addEventListener")
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
