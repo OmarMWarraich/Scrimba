78ac1ae09a85658d04818fc01ccb3dd7b15aabd2
OmarMWarraich <earthling986@gmail.com>
2022-02-12 17:57:44 +0500

    Generated sentence using template string

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -1,51 +1,33 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
-const deleteBtn = document.getElementById("delete-btn")
-const tabBtn = document.getElementById("tab-btn")
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+// The generateSentence(desc, arr) takes two parameterer: a description and an array.
+// It should return a string based upon the description and array.
 
+// Example 1: if you pass in "largest countries",and ["China", "India", "USA"],
+// it should return the string: "The 3 largest countries are China, India, USA"
 
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
+// Example 2: If you pass in "best fruits" and ["Apples", "Bananas"], it should return:
+// "The 2 best fruits are Apples, Bananas"
+
+// Use both a for loop and a template string to solve the challenge
+
+function generateSentence(desc, arr) {
+    let baseString = `The ${arr.length} ${desc} are `
+    const lastIndex = arr.length - 1
+    const secondLastIndex = lastIndex - 1
+    for (let i = 0; i < arr.length; i++) {
+        if (i === secondLastIndex) {
+            baseString += arr[i] + ' and '
+        }
+        else if (i === lastIndex) {
+            baseString += arr[i] + '.'
+        } else {
+            baseString += arr[i] + ', '    
+        }        
+    }
+    return baseString
 }
 
-tabBtn.addEventListener("click", function () {    
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        render(myLeads)
-    })    
-})
+const sentence = generateSentence("highest mountains", ["Mountain Everest", "K2"])
+const sentence1 = generateSentence("best fruits", ["Apple", "Bananas"])
 
-function render(leads) {
-    let listItems = ""
-    for (let i = 0; i < leads.length; i++) {
-        listItems += `
-            <li>
-                <a target='_blank' href='${leads[i]}'>
-                    ${leads[i]}
-                </a>
-            </li>
-    `
-    }
-    ulEl.innerHTML = listItems
-    }
-
-deleteBtn.addEventListener("dblclick", function () {
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
-
-})
-
-inputBtn.addEventListener("click", function () {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
-
-})
-
+console.log(sentence)
+console.log(sentence1)
