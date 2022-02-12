3d7bab534bd4d9019fb9a9c1a0adefeeab81b4f5
OmarMWarraich <earthling986@gmail.com>
2022-02-12 19:59:30 +0500

    MyEmojis: adding the input and button

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -2,6 +2,19 @@
 
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
 
+const emojiContainer = document.getElementById('emojiContainer')
+
+
 for (let i = 0; i < myEmojis.length; i++) {
-    console.log(myEmojis[i])
+    // emojiContainer.innerHTML += `<span>${myEmojis[i]}</span>`
+    const emoji = document.createElement('span')
+    emoji.textContent = myEmojis[i]
+    emojiContainer.append(emoji)
 }
+
+const pushBtn = document.getElementById('push-btn')
+
+pushBtn.addEventListener('click', function () {
+    const emojiInput = document.getElementById('emoji-input')
+    console.log(emojiInput.value)
+})
