1f8ae37b03a576ec7fe9607de8fb1f40ae8f25e3
OmarMWarraich <earthling986@gmail.com>
2022-02-12 15:20:45 +0500

    Refactor the function to include parameters

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -1,21 +1,34 @@
 let myLeads = []
-
+let oldLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+    `
+    }
+    ulEl.innerHTML = listItems
+    }
 
 deleteBtn.addEventListener("dblclick", function () {
     localStorage.clear()
     myLeads = []
-    renderLeads()
+    render(myLeads)
 
 })
 
@@ -23,20 +36,7 @@ inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
+    render(myLeads)
 
 })
 
-function renderLeads() {
-let listItems = ""
-for (let i = 0; i < myLeads.length; i++) {
-    listItems += `
-        <li>
-            <a target='_blank' href='${myLeads[i]}'>
-                ${myLeads[i]}
-            </a>
-        </li>
-`
-}
-ulEl.innerHTML = listItems
-}
