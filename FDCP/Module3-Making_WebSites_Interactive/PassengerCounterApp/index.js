26e3151c108b3bc9772a23836dbce08fd1499f6e
OmarMWarraich <earthling986@gmail.com>
2022-02-12 14:13:21 +0500

    Stored, saved and got the values from localStorage
    localStorage only works with strings, therefore, JSON.stringify used to convert the array to string. Conversely, JSON.parse used to convert string to arrays as .push method works on arrays only

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -1,13 +1,28 @@
-let myLeads = []
+let myLeads = `["www.awesomelead.com"]`
+
+myLeads = JSON.parse(myLeads)
+
+myLeads.push("www.epiclead.com")
+
+console.log(myLeads)
+
+myLeads = JSON.stringify(myLeads)
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
 const inputEl = document.getElementById("input-el")
 
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+
+
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify("myLeads"))
     renderLeads()
+
 })
 
 function renderLeads() {
