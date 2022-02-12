40b21d05bb01e6c30572e540c02ea6120d8db730
OmarMWarraich <earthling986@gmail.com>
2022-02-12 19:32:22 +0500

    log the emojis

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -1,3 +1,7 @@
 // 1. Loop through the myEmojis array and log each of them out to the console
 
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
+
+for (let i = 0; i < myEmojis.length; i++) {
+    console.log(myEmojis[i])
+}
