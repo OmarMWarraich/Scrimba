3c06b058408d3b5c3febfe7171ace2e728d7f150
OmarMWarraich <earthling986@gmail.com>
2022-02-14 00:46:09 +0500

    Dice Game
    Setup game state and DOM nodes, rolled the dice, switched turns, displayed the dice no, kept track of the score, declared a winner, refactored, reset game

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -1,46 +1,67 @@
-/*
-    Grabbing all carousel items and attaching button event listeners
+// Create variables for the game state
+let player1Score = 0
+let player2Score = 0
+let player1Turn = true
+
+// Create variables to store references to the necessary DOM nodes
+const player1Dice = document.getElementById("player1Dice")
+const player2Dice = document.getElementById("player2Dice")
+const player1Scoreboard = document.getElementById("player1Scoreboard")
+const player2Scoreboard = document.getElementById("player2Scoreboard")
+const message = document.getElementById("message")
+const rollBtn = document.getElementById("rollBtn")
+const resetBtn = document.getElementById("resetBtn")
+
+function showResetButton() {
+    rollBtn.style.display = "none"
+    resetBtn.style.display = "block"
+}
+
+/* Hook up a click event listener to the Roll Dice Button. */
+ rollBtn.addEventListener("click", function() {
+    const randomNumber = Math.floor(Math.random() * 6) + 1
+
+    if (player1Turn) {
+        player1Score += randomNumber
+        player1Scoreboard.textContent = player1Score
+        player1Dice.textContent = randomNumber
+        player1Dice.classList.remove("active")
+        player2Dice.classList.add("active")
+        message.textContent = "Player 2 Turn"
+    } else {
+        player2Score += randomNumber
+        player2Scoreboard.textContent = player2Score
+        player2Dice.textContent = randomNumber
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")
+        message.textContent = "Player 1 Turn"
+    }
     
-    1) Grab all 'carousel-item' - hint: elements...by...class name! Store this value in a const called 'slides'
-    2) Set a slide position of 0 (as that's what the starting index position will be when we grab all the carousel items - remember that arrays start from 0 and not 1) - we want the slide position to always start at 0. When a user goes to the next or previous slide, we will update this--the reason it's 0 and not one is that we want to match the position in the 'slides' const that we stored above! As this is going to be changing, what seems more appropriate, a let statement or a const?
-    3) Store a const value of 'totalSlides' that is equal to the length of the slides
-    4) Add event listeners to both buttons 'carousel-button-next' and 'carousel-button-prev' - seeing as though we expect the user to click to change slide position, make sure we set the eventListener to 'click' and the second arguments will be functions!
-    5) Create two functions: moveToNextSlide and moveToPrevSlide - use both of these in action 4 as the second argument on your 'addEventListener' - don't worry about these functions being empty, we will add to them in the upcoming screencasts
-    6) Use console.log to validate that you have 3 'carousel-item' in your 'slides' const
-*/
-
-const slides = document.getElementsByClassName('carousel-item');
-let slidePosition = 0;
-const totalSlides = slides.length;
-
-const hideAllSlides = () => {
-    for (let slide of slides) {
-        slide.classList.remove('carousel-item-visible');
-        slide.classList.add('carousel-item-hidden');
+    if (player1Score >= 20) {
+        message.textContent = "Player 1 Won 🥳"
+        showResetButton()
+    }  else if (player2Score >= 20) {
+        message.textContent = "Player 2 Won 🎉"
+        showResetButton()
     }
+    player1Turn = !player1Turn
+})
+ 
+resetBtn.addEventListener("click", function(){
+    reset()
+})
+
+function reset() {
+    player1Score = 0
+    player2Score = 0
+    player1Turn = true
+    player1Scoreboard.textContent = 0
+    player2Scoreboard.textContent = 0
+    player1Dice.textContent = "-"
+    player2Dice.textContent = "-"
+    message.textContent = "Player 1 Turn"
+    resetBtn.style.display = "none"
+    rollBtn.style.display = "block"
+    player2Dice.classList.remove("active")
+    player1Dice.classList.add("active")
 }
-
-const moveToNextSlide = () => {
-    hideAllSlides();
-    if (slidePosition === totalSlides - 1) {
-        slidePosition = 0;
-    } else {
-        slidePosition++;
-    }
-    slides[slidePosition].classList.add('carousel-item-visible');
-}
-
-const moveToPrevSlide = () => {
-    hideAllSlides();
-    if (slidePosition === 0) {
-        slidePosition = totalSlides - 1;
-    } else {
-        slidePosition--;
-    }
-    slides[slidePosition].classList.add('carousel-item-visible');
-}
-
-document.getElementById('carousel-button-next').addEventListener('click', moveToNextSlide)
-document.getElementById('carousel-button-prev').addEventListener('click', moveToPrevSlide)
-
-
