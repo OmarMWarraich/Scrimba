aab278ec2447da54715e9ffee899bfb3b58e515b
OmarMWarraich <earthling986@gmail.com>
2022-02-14 03:18:46 +0500

    Initial commit: snake game
    Set up HTNL, added CSS, picked out elements using document.querySelector(), added squares into the grid using js, drew the snake on the grid and used pop() to remove the tail of snake

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -1,67 +1,37 @@
-// Create variables for the game state
-let player1Score = 0
-let player2Score = 0
-let player1Turn = true
+const grid = document.querySelector('.grid')
+const startButton = document.getElementById('start')
+const score = document.getElementById('score')
+let squares = []
+let currentSnake = [2,1,0]
 
-// Create variables to store references to the necessary DOM nodes
-const player1Dice = document.getElementById("player1Dice")
-const player2Dice = document.getElementById("player2Dice")
-const player1Scoreboard = document.getElementById("player1Scoreboard")
-const player2Scoreboard = document.getElementById("player2Scoreboard")
-const message = document.getElementById("message")
-const rollBtn = document.getElementById("rollBtn")
-const resetBtn = document.getElementById("resetBtn")
-
-function showResetButton() {
-    rollBtn.style.display = "none"
-    resetBtn.style.display = "block"
-}
-
-/* Hook up a click event listener to the Roll Dice Button. */
- rollBtn.addEventListener("click", function() {
-    const randomNumber = Math.floor(Math.random() * 6) + 1
-
-    if (player1Turn) {
-        player1Score += randomNumber
-        player1Scoreboard.textContent = player1Score
-        player1Dice.textContent = randomNumber
-        player1Dice.classList.remove("active")
-        player2Dice.classList.add("active")
-        message.textContent = "Player 2 Turn"
-    } else {
-        player2Score += randomNumber
-        player2Scoreboard.textContent = player2Score
-        player2Dice.textContent = randomNumber
-        player2Dice.classList.remove("active")
-        player1Dice.classList.add("active")
-        message.textContent = "Player 1 Turn"
+function createGrid() {
+    //create 100 of these elements with a for loop
+    for (let i=0; i < 100; i++) {
+     //create element
+    const square = document.createElement('div')
+    //add styling to the element
+    square.classList.add('square')
+    //put the element into our grid
+    grid.appendChild(square)
+    //push it into a new squares array    
+    squares.push(square)
     }
+}
+createGrid()
+
+currentSnake.forEach(index => squares[index].classList.add('snake'))
+
+function move() {
+    //remove last element from our currentSnake array
+    const tail = currentSnake.pop()
+    console.log(tail)
+    console.log(currentSnake)
+    //remove styling from last element
+    squares[tail].classList.remove('snake')
+    //add sqaure in direction we are heading
     
-    if (player1Score >= 20) {
-        message.textContent = "Player 1 Won 🥳"
-        showResetButton()
-    }  else if (player2Score >= 20) {
-        message.textContent = "Player 2 Won 🎉"
-        showResetButton()
-    }
-    player1Turn = !player1Turn
-})
- 
-resetBtn.addEventListener("click", function(){
-    reset()
-})
-
-function reset() {
-    player1Score = 0
-    player2Score = 0
-    player1Turn = true
-    player1Scoreboard.textContent = 0
-    player2Scoreboard.textContent = 0
-    player1Dice.textContent = "-"
-    player2Dice.textContent = "-"
-    message.textContent = "Player 1 Turn"
-    resetBtn.style.display = "none"
-    rollBtn.style.display = "block"
-    player2Dice.classList.remove("active")
-    player1Dice.classList.add("active")
+    //add styling so we can see it
+    
 }
+move()
+
