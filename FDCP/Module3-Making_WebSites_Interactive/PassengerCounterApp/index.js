4a8d1f2794b9d954a9c8796d693591a16623dc73
OmarMWarraich <earthling986@gmail.com>
2022-02-14 00:12:40 +0500

    Final version: carousel
    added html structure and styling, used translateY to persfectly align buttons, moved to next slide, hid non active slides, moved to previous slides, transitioned smoothly using CSS keyframes, Todo: set height, automate slide transitions, add text to the slides, show the user which slide they are on

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -1,16 +1,46 @@
-document.getElementById('searchInput').addEventListener('keyup', (event) => {
-    let searchQuery = event.target.value.toLowerCase();
-    let allNamesDOMCollection = document.getElementsByClassName('name')
+/*
+    Grabbing all carousel items and attaching button event listeners
     
-    for (let i = 0; i < allNamesDOMCollection.length; i++){
-        const currentName = allNamesDOMCollection[i].textContent.toLowerCase();
-        console.log(currentName)
+    1) Grab all 'carousel-item' - hint: elements...by...class name! Store this value in a const called 'slides'
+    2) Set a slide position of 0 (as that's what the starting index position will be when we grab all the carousel items - remember that arrays start from 0 and not 1) - we want the slide position to always start at 0. When a user goes to the next or previous slide, we will update this--the reason it's 0 and not one is that we want to match the position in the 'slides' const that we stored above! As this is going to be changing, what seems more appropriate, a let statement or a const?
+    3) Store a const value of 'totalSlides' that is equal to the length of the slides
+    4) Add event listeners to both buttons 'carousel-button-next' and 'carousel-button-prev' - seeing as though we expect the user to click to change slide position, make sure we set the eventListener to 'click' and the second arguments will be functions!
+    5) Create two functions: moveToNextSlide and moveToPrevSlide - use both of these in action 4 as the second argument on your 'addEventListener' - don't worry about these functions being empty, we will add to them in the upcoming screencasts
+    6) Use console.log to validate that you have 3 'carousel-item' in your 'slides' const
+*/
 
-        if (currentName.includes(searchQuery)) {
-            allNamesDOMCollection[i].style.display = 'block';
-        } else {
-            allNamesDOMCollection[i].style.display = 'none';
-        }
+const slides = document.getElementsByClassName('carousel-item');
+let slidePosition = 0;
+const totalSlides = slides.length;
+
+const hideAllSlides = () => {
+    for (let slide of slides) {
+        slide.classList.remove('carousel-item-visible');
+        slide.classList.add('carousel-item-hidden');
     }
-})
-    
+}
+
+const moveToNextSlide = () => {
+    hideAllSlides();
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+    slides[slidePosition].classList.add('carousel-item-visible');
+}
+
+const moveToPrevSlide = () => {
+    hideAllSlides();
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+    slides[slidePosition].classList.add('carousel-item-visible');
+}
+
+document.getElementById('carousel-button-next').addEventListener('click', moveToNextSlide)
+document.getElementById('carousel-button-prev').addEventListener('click', moveToPrevSlide)
+
+
