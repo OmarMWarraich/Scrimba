3501b10c49777f62f391635cc8c82b9ceb146ea2
OmarMWarraich <earthling986@gmail.com>
2022-02-14 04:10:16 +0500

    Used keyCodes, addEventListener(), added apple
    Used keyCodes 377-40 for control key manipulation, used addEventListeners to listen out for key presses /changing the snake's direction, defined games boundaries using modulus, and generated apples with a do while loop with Math.random()

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -2,11 +2,14 @@ const grid = document.querySelector('.grid')
 const startButton = document.getElementById('start')
 const score = document.getElementById('score')
 let squares = []
-let currentSnake = [2,1,0]
+let currentSnake = [2, 1, 0]
+let direction = 1
+const width = 10
+let appleIndex = 0
 
 function createGrid() {
     //create 100 of these elements with a for loop
-    for (let i=0; i < 100; i++) {
+    for (let i=0; i < width*width; i++) {
      //create element
     const square = document.createElement('div')
     //add styling to the element
@@ -21,11 +24,20 @@ createGrid()
 
 currentSnake.forEach(index => squares[index].classList.add('snake'))
 
+let timerId = setInterval(move, 1000)
+
 function move() {
+    if (
+        
+        (currentSnake[0] + width >= width*width && direction === 10) || // snake has hit bottom
+        (currentSnake[0] % width === width-1 && direction === 1) || // snake has hit right
+        (currentSnake[0] % width === 0 && direction === -1) || // snake has hit left
+        (currentSnake[0] - width < 0 && direction === -10) || // snake has hit top
+        squares[currentSnake[0] + direction].classList.contains('snake')
+    )
+    return clearInterval(timerId)
     //remove last element from our currentSnake array
     const tail = currentSnake.pop()
-    console.log(tail)
-    console.log(currentSnake)
     //remove styling from last element
     squares[tail].classList.remove('snake')
     //add sqaure in direction we are heading
@@ -35,4 +47,32 @@ function move() {
 }
 move()
 
-let timerId = setInterval(move, 1000)
+function generateApples() {
+    do {
+        // generate a random no.
+        appleIndex = Math.floor(Math.random() * squares.length)
+    } while (squares[appleIndex].classList.contains('snake'))
+    squares[appleIndex].classList.add('apple')
+}
+generateApples()
+// 39 is right arrow
+// 38 is for the up arrow
+// 37 is for the left arrow
+// 40 is for the down arrow
+
+function control(e) {
+    if (e.keyCode === 39) {
+        console.log('right pressed')
+        direction = 1
+    } else if (e.keyCode === 38) {
+        console.log('up pressed')
+        direction = -10
+    } else if (e.keyCode === 37) {
+        console.log('left pressed')
+        direction = -1
+    } else if (e.keyCode === 40) {
+        console.log('down pressed')
+        direction = +width
+    }
+}
+document.addEventListener('keyup', control)
