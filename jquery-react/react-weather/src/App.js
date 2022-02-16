3d923982d4f2f74e36712a5d66e7b912ab8ebf63
OmarMWarraich <earthling986@gmail.com>
2022-02-16 19:12:27 +0500

    Connected to the API

--- jquery-react/react-weather/src/App.js
+++ jquery-react/react-weather/src/App.js
@@ -1,13 +1,23 @@
+import React from 'react';
 import Header from './components/Header';
 import WeatherSearch from './components/WeatherSearch'
 
-function App() {
+class App extends React.Component {
+  api_call = async () => {
+    const API_KEY = "9c3cb98520f309bd159e77512e8e5e28"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}&units=metric`
+    const request = await fetch(url)
+    const response = await request.json
+    console.log("Success!")
+  }
+  render () {
   return (
     <div className="App">
       <Header />
       <WeatherSearch />
     </div>
   );
+}
 }
 
 export default App;
