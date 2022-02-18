64f50f532cdc3974fb26e75abef0cab3d0c992ac
OmarMWarraich <earthling986@gmail.com>
2022-02-18 18:07:09 +0500

    Seperate eventlistener callback

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -1,13 +1,13 @@
 /**
- Challenge: Add a button that, when clicked, gets a new deck of cards from the deckofcards API
- 
- URL: https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/
- 
- Log the whole response to the console
+ * Challenge: Define our anonymous callback function as a separate function, then
+ * pass it as the 2nd parameter to our `addEventListener`
  */
 
-document.getElementById('new-deck').addEventListener("click", function () {
+
+function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
-        .then(data => console.log(data));
-})
+        .then(data => console.log(data))
+}
+
+ document.getElementById("new-deck").addEventListener("click", handleClick)
