adc160d9b8c0ab2d33c5ec28551d95980902d3a2
OmarMWarraich <earthling986@gmail.com>
2022-02-18 17:53:21 +0500

    Callback Setup

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -6,3 +6,8 @@
  Log the whole response to the console
  */
 
+document.getElementById('new-deck').addEventListener("click", function () {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => console.log(data));
+})
