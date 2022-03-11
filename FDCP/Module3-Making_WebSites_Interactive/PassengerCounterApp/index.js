2404c86e5eb163795cc31fd67f1c832d395ccb2e
OmarMWarraich <earthling986@gmail.com>
2022-03-12 01:27:51 +0500

    setTimeout and array.filter method

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -10,4 +10,49 @@ function handleClick() {
         .then(data => console.log(data))
 }
 
- document.getElementById("new-deck").addEventListener("click", handleClick)
+document.getElementById("new-deck").addEventListener("click", handleClick)
+ 
+/**
+ * Challenge: 
+ * 
+ * Part 1: write a `setTimeout` command. Have it wait for 2000 ms before logging "I finally ran!" to the console
+ * 
+ * Part 2: Move the anonymous in-line function to its own, named function
+ */
+
+function callback() {
+     console.log("I finally ran!")
+ }   
+
+setTimeout(callback, 2000)
+ 
+/**
+ * Challenge: 
+ * 
+ * Part 1: Given the array of objects below, create a new array with the `.filter()` array method that contains only the objects where "hasPet" is true
+ * 
+ * 
+ * Part 2: Move the anonymous in-line function to its own, named function
+ */
+
+ const people = [
+    { name: "Jack", hasPet: true },
+    { name: "Jill", hasPet: false },
+    { name: "Alice", hasPet: true },
+    { name: "Bob", hasPet: false },
+ ]
+
+ 
+
+const peopleWithPets = people.filter(person => person.hasPet) 
+
+console.log(peopleWithPets)
+
+// https://www.javascripttutorial.net/javascript-array-filter
+
+/**
+ * Extra challenge to practice array.filter:
+ * 
+ * Using .filter, create a new array of people who are 18 and older
+ * (should be Jill, Alice, and Bob)
+ */
