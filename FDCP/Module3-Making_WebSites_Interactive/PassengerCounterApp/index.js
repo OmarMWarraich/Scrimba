08b3cba58839a3959e74ef53f6fb78a8efc48568
OmarMWarraich <earthling986@gmail.com>
2022-03-12 02:46:42 +0500

    saved deck_id for later use

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -1,13 +1,11 @@
-/**
- * Challenge: Define our anonymous callback function as a separate function, then
- * pass it as the 2nd parameter to our `addEventListener`
- */
-
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data)
+            deckId = data.deck_id
+    })
 }
 
 document.getElementById("new-deck").addEventListener("click", handleClick)
@@ -15,59 +13,13 @@ document.getElementById("new-deck").addEventListener("click", handleClick)
 /**
  * Challenge: 
  * 
- * Part 1: write a `setTimeout` command. Have it wait for 2000 ms before logging "I finally ran!" to the console
+ * Background:
+ * The Deck of cards API expects us to provide the deck id
+ * of the deck we're playing with so it can remember which
+ * cards we've already drawn, how many are remaining in the
+ * deck, etc.
  * 
- * Part 2: Move the anonymous in-line function to its own, named function
+ * Task: save the deck_id from the returned data to a local
+ * variable so we can use it later
  */
 
-function callback() {
-     console.log("I finally ran!")
- }   
-
-setTimeout(callback, 2000)
- 
-/**
- * Challenge: 
- * 
- * Part 1: Given the array of objects below, create a new array with the `.filter()` array method that contains only the objects where "hasPet" is true
- * 
- * 
- * Part 2: Move the anonymous in-line function to its own, named function
- */
-
- const people = [
-    { name: "Jack", hasPet: true },
-    { name: "Jill", hasPet: false },
-    { name: "Alice", hasPet: true },
-    { name: "Bob", hasPet: false },
- ]
-
- 
-
-// const peopleWithPets = people.filter(person => person.hasPet) 
-
-// console.log(peopleWithPets)
-
-function filterArray(array, callback) {
-    const resultingArray = []
-    // Write the filtering logic
-    for (let item of array) {
-        const shouldbeIncluded = callback(item)
-        if (shouldbeIncluded) {
-            resultingArray.push(item)
-        }
-    }
-    return resultingArray
-}
-
-/**
- * Challenge: Use the filter array method!
- * Given the above `people` array, return a new array with only people where `hasPet` is true.
- * Note: The callback function will be given the individual item in the array for a parameter.
- */
-
-const peopleWithPets = filterArray(people, function (person) {
-    return person.hasPet
-})
-
-console.log(peopleWithPets)
