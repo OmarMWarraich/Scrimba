c4cae01225ff002aafa5e6ad95fbcf15bbb62ec1
OmarMWarraich <earthling986@gmail.com>
2022-03-12 03:19:19 +0500

    displayed the card images

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -11,27 +11,22 @@ function handleClick() {
 
 document.getElementById("new-deck").addEventListener("click", handleClick)
  
-/**
- * Challenge: 
- * 
- * Task: Using the saved deckId,draw 2 new cards from the deck
- * 
- * Docs for original deck of cards API: https://deckofcardsapi.com/#draw-card
- * BaseUrl you'll use: https://apis.scrimba.com/deckofcards/api/deck/
- * (that will replace the base url of https://deckofcardsapi.com/api/deck/)
- * that you'll see in the deck of cards API docs.
- * 
- * 1. Create a new button that, when clicked, draws 2 cards from the the deckId
- * you have saved
- *      Note: you'll need to get a new deck every time you refresh the page.
- *      since you're only saving your deckId in a local variable right now
- * 2. Log those 2 cards to the console.
- */
-
-
 document.getElementById("draw-cards").addEventListener("click", () => {
     console.log(deckId)
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data.cards)
+            document.getElementById("cards").innerHTML = `
+                <img src=${data.cards[0].image} />
+                <img src=${data.cards[1].image} />
+            `
+        })
 })
+
+/**
+ * Challenge:
+ * 
+ * Display th images of the 2 cards in the browser.
+ * Use `innerHTML``to insert a couple <img> elements on the page.
+ */
