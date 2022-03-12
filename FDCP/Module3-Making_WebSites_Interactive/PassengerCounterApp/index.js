b083681cd2023afcab5ea25aabe3f80995137651
OmarMWarraich <earthling986@gmail.com>
2022-03-12 15:34:13 +0500

    War- refactored card image placement
    Placed each of the cards into its respective card-slot using element.children in the DOM instead of giving eachcard-slot its own unique ID

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -1,4 +1,7 @@
 let deckId
+const cardsContainer = document.getElementById("cards");
+const newDeckBtn = document.getElementById("new-deck");
+const drawCardBtn = document.getElementById("draw-cards");
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
@@ -9,17 +12,19 @@ function handleClick() {
     })
 }
 
-document.getElementById("new-deck").addEventListener("click", handleClick)
+newDeckBtn.addEventListener("click", handleClick)
  
-document.getElementById("draw-cards").addEventListener("click", () => {
+drawCardBtn.addEventListener("click", () => {
     console.log(deckId)
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
             console.log(data.cards)
-            document.getElementById("cards").innerHTML = `
-                <img src=${data.cards[0].image} />
-                <img src=${data.cards[1].image} />
+            cardsContainer.children[0].innerHTML = `
+                <img src=${data.cards[0].image} class="card" />
+            `
+            cardsContainer.children[1].innerHTML = `
+                <img src=${data.cards[1].image} class="card" />
             `
         })
 })
