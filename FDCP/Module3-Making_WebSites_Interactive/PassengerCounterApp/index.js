73eda3d84f782f55857caac3ab6c2dee1205c103
OmarMWarraich <earthling986@gmail.com>
2022-03-12 17:06:50 +0500

    War- determined the winning card -part-1
    Determined the winner card, Created a function with parameters card1 and card2

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -29,3 +29,27 @@ drawCardBtn.addEventListener("click", () => {
         })
 })
 
+function determineCardWinner(card1, card2) {
+    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"]
+    const card1ValueIndex = valueOptions.indexOf(card1).value
+    const card2ValueIndex = valueOptions.indexOf(card2).value
+    console.log("card 1:", card1ValueIndex)
+    console.log("card 2:", card2ValueIndex)
+
+    if (card1ValueIndex > card2ValueIndex) {
+        console.log("Card 1 wins!")
+    } else if (card1ValueIndex < card2ValueIndex) {
+        console.log("Card 2 wins!")
+    } else {
+        console.log("It's a tie!")
+    }
+}
+
+const card1Obj = {
+    value: "7"
+}
+const card2Obj = {
+    value: "KING"
+}
+
+determineCardWinner(card1Obj, card2Obj)
