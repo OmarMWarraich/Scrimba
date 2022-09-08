2ab366b4d0321d729cc5d3a620c89b5b669d3f76
OmarMWarraich <earthling986@gmail.com>
2022-09-08 20:54:31 +0500

    feat: BlogSpace-GET first 5 posts

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -1,14 +1,20 @@
 /**
- * Challenge:
- * 
- * Send a request to the JSON Placeholder API using `fetch`
- * URL: https://apis.scrimba.com/jsonplaceholder/posts
- * 
- * Documentation: https://jsonplaceholder.typicode.com/
- * 
- * Log the response data to the console
+ Challenge:
+ 
+ GET a list of blog posts from the JSON Placeholder API.
+ 
+ BaseURL: https://apis.scrimba.com/jsonplaceholder/
+ Endpoint: /posts
+ 
+ Since there's so many posts, let's limit the array to just 5 items.
+ You can use the `.slice()` array method to just grab the first 5 objects
+ from the data array that comes back from the API
  */
 
-fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-    .then(response => response.json())
-    .then(data => console.log(data))
+ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+        .then(response => response.json())
+        .then(data => {
+            const fivePosts = data.slice(0, 5)
+            console.log(fivePosts)
+        })
+ 
