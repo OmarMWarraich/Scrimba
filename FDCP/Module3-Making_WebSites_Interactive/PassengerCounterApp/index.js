7bbcaf498888c153237a7946eddae1cadaa01bad
OmarMWarraich <earthling986@gmail.com>
2022-09-08 20:26:31 +0500

    feat: fetch log data

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -8,3 +8,7 @@
  * 
  * Log the response data to the console
  */
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+    .then(response => response.json())
+    .then(data => console.log(data))
