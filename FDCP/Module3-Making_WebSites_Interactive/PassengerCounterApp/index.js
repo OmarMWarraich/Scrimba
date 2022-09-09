13c1e2d4056de5c74fbff57debd181c4f230b9b5
OmarMWarraich <earthling986@gmail.com>
2022-09-09 20:14:55 +0500

    feat: Blogspace- send post server add posts list

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -1,23 +1,46 @@
-/**
- * Challenge:
- * 
- * Check this website for how to change the Content-Type header
- * to indicate that there's JSON in the request body.
- * 
- * https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#supplying_request_options
- * 
- * Apply that header to our fetch request, send it, and check what comes back.
- */
-
- fetch("https://apis.scrimba.com/jsonplaceholder/todos", {
-    method: "POST",
-    body: JSON.stringify({
-        title: "Buy Milk",
-        completed: false
-    }),
-    headers: {
-        "Content-Type": "application/json"
-    }
-})
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        const postsArr = data.slice(0, 5)
+        let html = ""
+        for (let post of postsArr) {
+            html += `
+                <h3 class="blah">${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+            `
+        }
+        document.getElementById("app").innerHTML = html
+    })
+
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault()
+    const postTitle = document.getElementById("post-title").value
+    const postBody = document.getElementById("post-body").value
+    const data = {
+        title: postTitle,
+        body: postBody
+    }
+    
+    const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+        .then(res => res.json())
+        .then(post => {
+            /**
+             * Challenge: Update the DOM with the new blog entry
+             */
+            document.getElementById("app").innerHTML = `
+                <h3 class="blah">${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+                ${document.getElementById("app").innerHTML}
+            `
+        })
+})
