1ebaf4c0802f455f54a6cdcc71bd365e6386b788
OmarMWarraich <earthling986@gmail.com>
2022-09-09 19:55:41 +0500

    feat: BlogSpace-Requests-body and headers

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -1,40 +1,23 @@
 /**
- Challenge:
- 
- GET a list of blog posts from the JSON Placeholder API.
- 
- BaseURL: https://apis.scrimba.com/jsonplaceholder/
- Endpoint: /posts
- 
- Since there's so many posts, let's limit the array to just 5 items.
- You can use the `.slice()` array method to just grab the first 5 objects
- from the data array that comes back from the API
+ * Challenge:
+ * 
+ * Check this website for how to change the Content-Type header
+ * to indicate that there's JSON in the request body.
+ * 
+ * https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#supplying_request_options
+ * 
+ * Apply that header to our fetch request, send it, and check what comes back.
  */
 
- fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-        .then(response => response.json())
-        .then(data => {
-            const fivePosts = data.slice(0, 5)
-            let html = ""
-            for (let post of fivePosts) {
-                html += `
-                    <div class="post">
-                        <h2>${post.title}</h2>
-                        <p>${post.body}</p>
-                        <hr />
-                    </div>
-                `
-            }
-            document.querySelector("#app").innerHTML = html
-        })
- 
-document.querySelector("form").addEventListener("submit", function(e) {
-    e.preventDefault()
-    const postTitle = document.getElementById("post-title").value
-    const postBody = document.getElementById("post-body").value
-    const data = {
-        title: postTitle,
-        body: postBody
+ fetch("https://apis.scrimba.com/jsonplaceholder/todos", {
+    method: "POST",
+    body: JSON.stringify({
+        title: "Buy Milk",
+        completed: false
+    }),
+    headers: {
+        "Content-Type": "application/json"
     }
-    console.log(data);
 })
+    .then(res => res.json())
+    .then(data => console.log(data))
