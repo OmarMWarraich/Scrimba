efe3b921fc96577a9882940b37d041fb191c9d72
OmarMWarraich <earthling986@gmail.com>
2022-09-09 20:57:55 +0500

    feat: URL params and query strings

--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/index.js
@@ -1,54 +1,10 @@
-let postsArray = []
-const titleInput = document.getElementById("post-title")
-const bodyInput = document.getElementById("post-body")
-const form = document.querySelector("form")
+/**
+ * Challenge: GET all the comments from the blog post with ID of 2 and log to the console
+ * 
+ * BaseURL: https://apis.scrimba.com/jsonplaceholder/
+ * Endpoint: ??? (Check JSON Placeholder docs: https://jsonplaceholder.typicode.com/guide/ and look for the "Listing nested resources" section)
+ */
 
-function renderPosts() {
-    let html = ""
-    for (let post of postsArray) {
-        html += `
-            <h3>${post.title}</h3>
-            <p>${post.body}</p>
-            <hr />
-        `
-    }
-    document.getElementById("app").innerHTML = html
-}
-
-fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-    .then(res => res.json())
-    .then(data => {
-        postsArray = data.slice(0, 5)
-        renderPosts()
-    })
-
-form.addEventListener("submit", function(e) {
-    e.preventDefault()
-    const postTitle = titleInput.value
-    const postBody = bodyInput.value
-    const data = {
-        title: postTitle,
-        body: postBody
-    }
-    
-    const options = {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }
-    
-    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
-        .then(res => res.json())
-        .then(post => {
-            postsArray.unshift(post)
-            renderPosts()
-            /**
-             * Challenge: clear the form out!
-             */
-            titleInput.value = ""
-            bodyInput.value = ""
-            // form.reset()
-        })
-})
+ fetch("https://apis.scrimba.com/jsonplaceholder/posts/2/comments")
+ .then(res => res.json())
+ .then(data => console.log(data))
--- FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/quiz.md
+++ FDCP/Module3-Making_WebSites_Interactive/PassengerCounterApp/quiz.md
@@ -21,3 +21,34 @@ It forgets the interaction after the response is sent.
     6. Remove the bike with an ID of 56 from the list of bikes?
        DELETE /bikes/56
        
+* Nested Resources
+
+1. How is a nested resource URL like /bikes/123/reviews
+   different from an endpoint like /reviews?
+   /bikes/123/reviews - return an array of reviews about that specific bike
+   /reviews - return an array of all reviews on the site
+  
+2. What URL might you use to GET the review with an ID of 5 on the bike
+   with the ID of 123?
+   /bikes/123/reviews/5
+
+3. Describe a "URL Parameter" in your own words:
+   Variable inside the URL that acts as a placeholder for the real value
+   (Oftentimes they replace the ID of the resource)
+
+* Query Strings
+
+At Mike's Bikes, they also sell bike racks (endpoint is /bikeracks).
+
+What would you expect the endpoints to be for the following tasks:
+
+1. Get a list of all bike racks sold on the site?
+/bikeracks
+
+2. Get a list of all bike racks available in the physical store right now?
+   (Assume a query called "available" that is a boolean true/false)
+/bikeracks?available=true  ==> {available: "true"} (Will be parsed as a string)
+
+3. Get a list of all "Thule"-brand bike racks that can hold 4 bikes?
+   (Assume there are "brand" and "numBikes" queries)
+/bikeracks?brand=thule&numBikes=4
